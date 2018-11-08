60837862ca8fc6a66ba3cf8315ce8f8597554973
hucongcong <flycc258@163.com>
2018-11-08 11:40:41 +0800

    登录组件完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,17 +4,18 @@
     <!-- :model="form" form对象用来收集表单中所有的数据 -->
     <!-- label-width：设置label的宽度 -->
     <!-- el-form-item：表单项 -->
-    <el-form :model="form" label-width="80px">
+    <!-- rules: 设置校验的规则 给每一个el-form-item设置一个prop属性 v-model的值一样 -->
+    <el-form :model="form" ref="form" status-icon :rules="rules" label-width="80px">
       <img src="../assets/avatar.jpg" alt="">
-      <el-form-item label="用户名">
+      <el-form-item label="用户名" prop="username">
         <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
+      <el-form-item label="密码" prop="password">
         <el-input v-model="form.password" placeholder="请输入密码" type="password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">登录</el-button>
-        <el-button>重置</el-button>
+        <el-button type="primary" @click="login">登录</el-button>
+        <el-button @click="reset">重置</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -22,13 +23,76 @@
 
 <script>
 // shift + tab : 往前按了一下tab
+// 引入axios
+import axios from 'axios'
 export default {
   data() {
     return {
       form: {
         username: '',
         password: ''
+      },
+      rules: {
+        // 对username进行校验
+        username: [
+          // 必填项
+          { required: true, message: '用户名不能为空', trigger: 'change' },
+          // 校验长度
+          { min: 3, max: 9, message: '长度在 3 到 9 个字符', trigger: 'change' }
+        ],
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'change' },
+          // 校验长度
+          {
+            min: 6,
+            max: 12,
+            message: '长度在 6 到 12 个字符',
+            trigger: 'change'
+          }
+        ]
       }
+    }
+  },
+  methods: {
+    // 重置表单
+    reset() {
+      // 获取到表单组件，调用resetFields
+      this.$refs.form.resetFields()
+    },
+    // 登录
+    login() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          // 发送ajax请求
+          // console.log('校验通过了')
+          axios({
+            url: 'http://localhost:8888/api/private/v1/login',
+            method: 'post',
+            data: this.form
+          }).then(res => {
+            if (res.data.meta.status === 200) {
+              // 登录成功
+              this.$message.success('登录成功')
+              // 存储token
+              localStorage.setItem('token', res.data.data.token)
+              // 跳转到Home组件
+              this.$router.push('/home')
+            } else {
+              // 登录失败
+              // this.$message(res.data.meta.msg)
+              // this.$message({
+              //   message: res.data.meta.msg,
+              //   type: 'error'
+              // })
+              this.$message.error(res.data.meta.msg)
+            }
+          })
+        } else {
+          // 校验没通过
+          console.log('没通过')
+          return false
+        }
+      })
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,12 @@ import Router from 'vue-router'
 // 导入Login组件
 import Login from '@/components/Login.vue'
 
+// 导入Home组件
+import Home from '@/components/Home.vue'
+
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -15,6 +18,31 @@ export default new Router({
     {
       path: '/login',
       component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
 })
+
+// 给路由对象设置导航守卫
+// to: 去哪儿
+// from: 从哪儿来
+// next() : 指定跳转到哪个组件  next():表示放行  next(false)  next('/login')
+router.beforeEach((to, from, next) => {
+  // console.log(to, from, next)
+  // 判断是否有token
+  const token = localStorage.getItem('token')
+  // console.log(to.path)
+  if (to.path === '/login' || token) {
+    // 有token 放行
+    next()
+  } else {
+    // 没有token, 去登录
+    next('/login')
+  }
+})
+
+// 导出路由对象
+export default router
